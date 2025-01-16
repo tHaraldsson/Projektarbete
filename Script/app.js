37aab86a5d7481932ba0ffbc61fc05a210f4fab4
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-01-16 20:53:33 +0100

    seventh

--- Script/app.js
+++ Script/app.js
@@ -8,22 +8,3 @@ mobilIkon.addEventListener("click", () => {
     console.log('Hamburgerikonen klickades!');
     mobilNav.classList.toggle("active");
 });
-
-emailjs.init("gbZbI9VieA3iRMqna"); // Ersätt med din Public Key
-
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Förhindra att sidan laddas om
-
-        // Skicka formulärdata via EmailJS
-        emailjs.sendForm("service_es269xs", "template_dkjwxx5", form)
-            .then(function (response) {
-                alert("Ditt meddelande har skickats! Jag återkommer så snart som möjligt.");
-                form.reset(); // Rensa formuläret efter skickat meddelande
-            }, function (error) {
-                alert("Ett fel uppstod: " + error.text);
-            });
-    });
-});
