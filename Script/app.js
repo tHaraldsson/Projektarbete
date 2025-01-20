6196115def3d7b3abdf576f6fcf601c799f1de94
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-01-20 14:01:28 +0100

    thirteenth

--- Script/app.js
+++ Script/app.js
@@ -1,14 +1,14 @@
 
-console.log("JavaScript laddat");
 
+// Hamburgerikon
 const mobilIkon = document.getElementById("mobilnav-ikon");
 const mobilNav = document.getElementById("mobilnav");
 
 mobilIkon.addEventListener("click", () => {
-    console.log('Hamburgerikonen klickades!');
     mobilNav.classList.toggle("active");
 });
 
+// Säkerställer att layout är uppdaterad
 window.addEventListener("load", function () {
     window.dispatchEvent(new Event('resize'));
 });
